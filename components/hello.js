7c86f616fa7a08cda1e6c1f3d5ab7ec9b4728f75
unknown <tbhavsar121@gmail.com>
2021-07-22 21:50:51 +0530

    directory tree mapping works fine

--- components/hello.js
+++
@@ -1,8 +0,0 @@
-const Hello = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
-};
-export default Hello;
--- pages/taxanomy/index.js
+++ pages/taxanomy/index.js
@@ -3,26 +3,77 @@ import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import SubTreeItem from "../../components/subTreeItem";
-import Hello from "../../components/hello";
 import { TreeItem } from "@material-ui/lab";
+import Styles from "../../styles/taxanomy.module.css";
+import { Style } from "@material-ui/icons";
+import Link from "next/link";
 
-const taxonomy = ({ items }) => {
+const getTreeItemsFromData = (treeItems) => {
+  // console.log(JSON.stringify(treeItems.treeItems.children));
+  // return "h";
+  console.log(treeItems);
+  return treeItems.map((treeItemData) => {
+    let children = undefined;
+    if (treeItemData.children && treeItemData.children.length > 0) {
+      children = getTreeItemsFromData(treeItemData.children);
+      return (
+        <TreeItem
+          key={treeItemData.id}
+          nodeId={treeItemData.id}
+          label={treeItemData.name.slice(
+            treeItemData.name.lastIndexOf(`${"\\"}`) + 1,
+            treeItemData.name.length
+          )}
+          children={children}
+        />
+      );
+    } else {
+      return (
+        <Link
+          href={`taxanomy/${treeItemData.name.slice(
+            treeItemData.name.lastIndexOf(`${"\\"}`) + 1,
+            treeItemData.name.length
+          )}`}
+        >
+          <TreeItem
+            onClick={() => {}}
+            key={treeItemData.id}
+            nodeId={treeItemData.id}
+            label={treeItemData.name.slice(
+              treeItemData.name.lastIndexOf(`${"\\"}`) + 1,
+              treeItemData.name.length
+            )}
+            children={children}
+          />
+        </Link>
+      );
+    }
+  });
+};
+const DataTreeView = (treeItems) => {
   return (
-    <main>
-      <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-      >
-        <TreeItem nodeId="0" label={items.name}>
-          {console.log(items)}
-          <SubTreeItem items={items.children} />
-        </TreeItem>
-      </TreeView>
-      <Hello />
-    </main>
+    <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {console.log(treeItems)}
+      {getTreeItemsFromData(treeItems.treeItems.children)}
+    </TreeView>
   );
 };
+
+function taxonomy({ items }) {
+  return (
+    <div className={Styles.container}>
+      {console.log(items)}
+      <h1 className={Styles.title}>Taxanomy</h1>
+      <main className={Styles.main}>
+        <DataTreeView treeItems={items} />
+      </main>
+    </div>
+  );
+}
+
 export async function getStaticProps() {
   const files = buildGraph("public/taxanomy");
 
