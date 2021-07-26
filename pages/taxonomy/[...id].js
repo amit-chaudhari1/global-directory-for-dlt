dc5cdb451a2d96fde3847044235a2a46cc760e61
Amit Chaudhari <amitchaudhari9121@gmail.com>
2021-07-26 14:06:22 +0530

    Typo fix and improve design

---
+++ pages/taxonomy/[...id].js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import Styles from "../../styles/markdown.module.css";
+import { useEffect, useState } from "react";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ReactMarkdown from "react-markdown";
+import ReactMarkdownComponents from "../../lib/ReactMarkdownComponents";
+
+const Comment = () => {
+	const router = useRouter();
+	let slug = router.query.id.join("/");
+	const [content, setContent] = useState({});
+	useEffect(() => {
+		getContent();
+	}, []);
+	async function getContent() {
+		var data = await fetch(`/api/${slug}`);
+		setContent(await data.json());
+	}
+	return (
+		<div className={Styles.layout}>
+			<a className={Styles.back} href="/taxonomy">
+				<ArrowBackIcon /> Back to List
+			</a>
+
+			<ReactMarkdown
+				children={content.data}
+				components={ReactMarkdownComponents(content.data)}
+			></ReactMarkdown>
+		</div>
+	);
+};
+
+export default Comment;
